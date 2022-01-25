5e8afe6077dbe664aa83e4ba9cbc074d3f5871fe
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2022-01-25 20:21:57 -0300

    feat: 🎸 portfolio template done
    index portfolio template done: Page with logic, interface > modules with
    UI.

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,36 @@
+const Header = () => {
+
+
+    return (
+       <header className="header">
+                
+                <nav>
+                    <ul>
+                        <li>
+                            <a>
+                                Início
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                skills
+                            </a>
+                        </li>
+                        <li>
+                            <a>
+                                Portfolio
+                            </a>
+                        </li>
+                        <li>
+                            <button>
+                                language
+                            </button>
+                            
+                        </li>
+                    </ul>
+                </nav>
+       </header>
+    )
+}
+
+export default Header;
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import PortfolioContent from '../infrastructure/modules/portfolioContent';
+
+const Portfolio: NextPage = () => {
+  return ( <PortfolioContent /> )
+}
+
+export default Portfolio;
